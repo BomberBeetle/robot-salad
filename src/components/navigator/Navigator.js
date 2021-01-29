d6ba8dd0d87175054e50ddad0fd0707a8db5c17b
BomberBeetle <gdreks@gmail.com>
2021-01-29 14:53:29 -0300

    homepage content and navbar color revision.

--- src/components/navigator/Navigator.js
+++ src/components/navigator/Navigator.js
@@ -11,7 +11,7 @@ class Navigator extends React.Component{
 
     render(){
         return(
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg">
             <Navbar.Brand href="/">Robot Salad!</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -16,7 +16,7 @@ function Homepage() {
             <h1>Welcome to Robot Salad!</h1>
             <p>This is my personal website, where i'm going to put all sortsa things! Mostly just showing off my work, but also fun experiments, gags and more. There's also a blog section if you want to keep up to date with the latest hijinks I've been up to. </p>
             <h3>cool, but who wrote this, exactly?</h3>
-            <p>Oh, <b>I'm so glad you asked.</b> I'm <b>André Luiz</b>, also known on the internet by the pseudonyms <b>BomberBeetle</b> (on <a href="https://github.com/BomberBeetle">GitHub</a>) and <b>xdre</b> (pretty much everywhere else). My pronouns are he/they (but I seriously doubt anyone is going to call me by "they" haha), and I come from São Paulo, Brazil.</p>
+            <p>Oh, <b>I'm so glad you asked.</b> I'm <b>André Luiz</b>, also known on the internet by the pseudonyms <b>BomberBeetle</b> (on <a href="https://github.com/BomberBeetle">GitHub</a>) and <b>xdre</b> (pretty much everywhere else). I come from São Paulo, Brazil, known as the land of the drizzle (and also the economic centre of Brazil).</p>
             <div className="text-center">
                 <img src={mugshot} className="rounded Mugshot" alt="The author."/>
                 <p><small>Whoa mama, who's this handsome devil?</small></p>
@@ -25,13 +25,17 @@ function Homepage() {
             <h3>This is getting pretty boring. Can I play  with something?</h3>
             <p>yeah, sure. here's a clicky switch</p>
             <ClickySwitch toggled={true}/>
-            <p>In fact, you can have two:</p>
+            <p>In fact, you can have two more:</p>
             <div className="switchHell">
               <ClickySwitch toggled={true}/> 
               <ClickySwitch toggled={false}/>
             </div>
+            <p>Have fun.</p>
+            {
+            //TODO: Write a personal paragraph.
+            }
             <h3>Thanks! What now?</h3>
-            <p>idk go eat grass or something</p>
+            <p>Well, there's still plenty to do here! As I mentioned earlier, you could go to <a href="#portfolio">my portfolio</a> to see some of my machinations, or you could visit the <a href="#blog">blog section</a> to read some of my ramblings. Also, if you want to have a chat, <a href="#contact">here</a> are some places you can hit me up!</p> 
         </Col>
     </Row>
 </Container>
